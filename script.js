118c7c1f900ec7fd7715b5490a30452577c54f2c
David Moody <david@davidxmoody.com>
2013-07-16 11:51:11 +0100

    Grid is generated by script

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+var generateGrid = function(width, height) {
+    // Clear grid before generating new one
+    // TODO: add individual styling to grid depending on dimensions
+    $grid = $('#shufflegrid');
+    $grid.empty();
+
+    for (var y=0; y<height; y++) {
+        var $tr = $("<tr></tr>");
+        $grid.append($tr);
+        for (var x=0; x<width; x++) {
+            var $cell = (x===width-1 && y===height-1) ? 
+                    $('<td><div class="cell empty"></div></td>') : 
+                    $('<td><div class="cell"><img src="images/Ammonite.gif"/></div></td>');
+            $tr.append($cell);
+        }
+    }
+};
+
 $(document).ready(function() {
+
+    // Setup initial grid
+    generateGrid(3, 3);
+
     $('.cell').click(function() {
         /* TODO: check for not the empty cell, if necessary? */
         var $img = $(this).children('img');
