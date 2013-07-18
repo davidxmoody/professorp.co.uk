2f350724f2d6e1246b86b5a26c505e20463b7375
David Moody <david@davidxmoody.com>
2013-07-18 17:38:19 +0100

    First properly working version, uses cliped image

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
 var SLIDING_DURATION = 100;
-var TILE_WIDTH = 202;
-var tiles;
 
-var generateGrid = function(width, height) {
+var tiles;
+var tileHeight, tileWidth;
+
+var generateGrid = function(width, height, columns, rows) {
+
     // Clear grid before generating new one
-    // TODO: add individual styling to grid/tiles depending on dimensions
     $grid = $('#shufflegrid');
     $grid.empty();
     tiles = [];
 
+    // Tile heights and widths include everything (border, padding, width)
+    tileWidth = Math.floor(width/columns);
+    tileHeight = Math.floor(height/rows);
+
+    // The width and heigh parameters are the desired height and width only, 
+    // actual height and width may vary to eliminate gaps.
+    var gridWidth = tileWidth*columns;
+    var gridHeight = tileHeight*rows;
+
+    console.log(gridWidth, gridHeight, tileWidth, tileHeight);
+
+    // Set the new dimensions of the grid
+    $grid.css("width", gridWidth);
+    $grid.css("height", gridHeight);
+
     // Generate grid and tiles
-    for (var y=0; y<height; y++) {
+    for (var y=0; y<rows; y++) {
         tiles[y] = [];
-        for (var x=0; x<width; x++) {
-            if (x===width-1 && y===height-1) {  // if this is the final cell
+        for (var x=0; x<columns; x++) {
+            if (x===columns-1 && y===rows-1) {  // if this is the final cell
                 tiles[y][x] = null;  //TODO don't use null, use hidden tile instead
             } else {
-                var $tile = $('<div class="tile" style="margin-top: '+y*TILE_WIDTH+'px; margin-left: '+x*TILE_WIDTH+'px;"><p>'+x+','+y+'</p></div>');
+                var $tile = $('<div class="tile"></div>');
+
+                // Set tile position
+                $tile.css("top", y*tileHeight);
+                $tile.css("left", x*tileWidth);
+
+                // Set tile dimensions (the -2 is for the 1px border/margin)
+                $tile.css("width", tileWidth-2);
+                $tile.css("height", tileHeight-2);
+
+                // Set the "correct position" of the tile when checking if solved
                 $tile.data("x", x);
                 $tile.data("y", y);
+
+                // Set the background image (clipped)
+                $tile.css("background-image", "url(images/Ammonite.gif)");
+                $tile.css("background-size", gridWidth+"px "+gridHeight+"px");
+                $tile.css("background-repeat", "no-repeat");  //TODO is this necessary?
+                $tile.css("background-position", (-1*tileWidth*x-1)+"px "+(-1*tileHeight*y-1)+"px");
+
                 $grid.append($tile);
                 tiles[y][x] = $tile;
             }
@@ -27,45 +60,61 @@ var generateGrid = function(width, height) {
 
     // Setup triggers for clicking on tiles
     $('.tile').click(function() {
-        // Get x and y positions of this tile and the empty tile
-        for (var y=0; y<tiles.length; y++) {
-            for (var x=0; x<tiles[0].length; x++) {
-                if (tiles[y][x]===null) {
-                    var emptyx = x, emptyy = y;
-                } else if (tiles[y][x].data("x")===$(this).data("x") && 
-                           tiles[y][x].data("y")===$(this).data("y")) {
-                    var thisx = x, thisy = y;
-                }
-            }
-        }
-
-        var animation = null;
-        // Check if tile is in a movable position
-        if (thisx===emptyx && thisy-emptyy===1) {  // Move this tile up
-            animation = { top: "-="+TILE_WIDTH };
-        } else if (thisx===emptyx && thisy-emptyy===-1) {  // Move this tile down
-            animation = { top: "+="+TILE_WIDTH };
-        } else if (thisx-emptyx===1 && thisy===emptyy) {  // Move this tile left
-            animation = { left: "-="+TILE_WIDTH };
-        } else if (thisx-emptyx===-1 && thisy===emptyy) {  // Move this tile right
-            animation = { left: "+="+TILE_WIDTH };
-        }
-
-        // Move the tile
-        // TODO: make animation synchronous using callbacks
-        if (animation !== null) {
-            $(this).animate(animation, SLIDING_DURATION);
-            tiles[emptyy][emptyx] = tiles[thisy][thisx];
-            tiles[thisy][thisx] = null;
-        }
-
-        if (isSolved()) alert("You did it!");
-            
+        tryMoveTile($(this));
     });
 
 };
 
+var tryMoveTile = function($tile) {
+    if ($tile === null) return;
+
+    // Get x and y positions of this tile and the empty tile
+    for (var y=0; y<tiles.length; y++) {
+        for (var x=0; x<tiles[0].length; x++) {
+            if (tiles[y][x]===null) {
+                var emptyx = x, emptyy = y;
+            } else if (tiles[y][x].data("x")===$tile.data("x") && 
+                       tiles[y][x].data("y")===$tile.data("y")) {
+                var thisx = x, thisy = y;
+            }
+        }
+    }
+
+    var animation = null;
+    // Check if tile is in a movable position
+    if (thisx===emptyx && thisy-emptyy===1) {  // Move this tile up
+        animation = { top: "-="+tileHeight };
+    } else if (thisx===emptyx && thisy-emptyy===-1) {  // Move this tile down
+        animation = { top: "+="+tileHeight };
+    } else if (thisx-emptyx===1 && thisy===emptyy) {  // Move this tile left
+        animation = { left: "-="+tileWidth };
+    } else if (thisx-emptyx===-1 && thisy===emptyy) {  // Move this tile right
+        animation = { left: "+="+tileWidth };
+    }
+
+    // Move the tile
+    // TODO: make animation synchronous using callbacks
+    if (animation !== null) {
+        $tile.animate(animation, SLIDING_DURATION);
+        tiles[emptyy][emptyx] = tiles[thisy][thisx];
+        tiles[thisy][thisx] = null;
+    }
+
+    console.log(isSolved());
+    return animation !== null;
+}
+
+var randomShuffle = function(numMoves) {
+    // Randomly select any tile from the grid and try to move it, only continue if a tile was actually moved
+    for (var i=0; i<numMoves;) {
+        var randomTile = tiles[Math.floor(Math.random()*tiles.length)][Math.floor(Math.random()*tiles[0].length)];
+        if (tryMoveTile(randomTile)) i++;
+    }
+}
+
 var isSolved = function() {
+    // For all tiles in the tiles array, check that their actual position matches their creation position
+    //TODO prevent moving a tile back to where it was immediately before 
     for (var y=0; y<tiles.length; y++) {
         for (var x=0; x<tiles[0].length; x++) {
             if (tiles[y][x] !== null && (tiles[y][x].data("x") !== x || tiles[y][x].data("y") !== y)) 
@@ -78,6 +127,9 @@ var isSolved = function() {
 $(document).ready(function() {
 
     // Setup initial grid
-    generateGrid(3, 3);
+    generateGrid(300, 300, 3, 3);
+
+    //TODO number of shuffles needs to be different depending on grid size
+    randomShuffle(50);
 
 });
